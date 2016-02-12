8e56ff07edb2036832b0763ba792e1aa134eff6a
Ali Rantakari <visit-with-web-browser@hasseg.org>
2016-02-12 10:31:52 +0200

    Add Dictionary.merged()

--- Sources/Utils.swift
+++ Sources/Utils.swift
@@ -11,3 +11,18 @@ import Foundation
 func dispatchAfterInterval(interval: NSTimeInterval, queue: dispatch_queue_t, block: () -> Void) {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(interval * Double(NSEC_PER_SEC))), queue, block);
 }
+
+extension Dictionary {
+    
+    /// Returns a copy of `self`, adding the key-value pairs
+    /// from `other`, overwriting existing entries if necessary.
+    ///
+    func merged(other: Dictionary?) -> Dictionary {
+        guard let other = other else { return self }
+        var copy = self
+        for (k, v) in other {
+            copy.updateValue(v, forKey: k)
+        }
+        return copy
+    }
+}
